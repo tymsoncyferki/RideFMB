21c63e24f511c86e57f81286f99281ed45067e7a
tymsoncyferki <tymekurban@gmail.com>
2023-03-10 12:16:21 +0100

    env file

--- ridefmb/settings.py
+++ ridefmb/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,17 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = "django-insecure-e%x=_!t*q0z7%s2s#*23p4_$405ts)l2+!9@bbn+z*0@i@rtk6"
-
-# todo: As far as best practices here:
-# The original suggestion of leveraging python-dotenv to programmatically call
-# for the needed credential instead of hardcoding them is a very good path and
-# one we strongly recommend folks start with.
-#
-# My feeling here is While it might be 'easier' to just hardcode things,
-# it is a bad habit ever to pick up. Using a .env file adds a little complexity,
-# but in the long run, all the folks using this tutorial will be set up for
-# long-term success if they adopt this philosophy of 'never hardcode secrets' earlier.
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
