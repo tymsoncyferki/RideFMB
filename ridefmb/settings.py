633a185a4327d0fc453d94205713051e87cb84b3
tymsoncyferki <tymekurban@gmail.com>
2023-03-11 19:05:22 +0100

    oracle database setup

--- ridefmb/settings.py
+++ ridefmb/settings.py
@@ -78,15 +78,13 @@ WSGI_APPLICATION = "ridefmb.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'ridefmb_throughout',
-    #     'USER': 'ridefmb_throughout',
-    #     'PASSWORD': os.getenv('DB_PASSWORD'),
-    #     'HOST': 's-3.h.filess.io',
-    #     'PORT': "3307"
-    # },
-    "default": {
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': 'admin',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD')
+    },
+    "backup": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
