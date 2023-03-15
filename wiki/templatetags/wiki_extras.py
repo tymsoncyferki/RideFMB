cc348c67baedceca2fac545dabeb91cdb971fdaa
tymsoncyferki <tymekurban@gmail.com>
2023-03-16 00:47:14 +0100

    ranking navigation

--- wiki/templatetags/wiki_extras.py
+++ wiki/templatetags/wiki_extras.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name="sub")
 def sub(value, arg):
     return value - arg
+
+
+@register.filter(name='times')
+def times(number):
+    return range(1, number+1)
